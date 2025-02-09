4830fabdc8e081d915903820dd705c70f55668f5
EsethCreatesLife <esethutradewarr43@gmail.com>
2025-02-10 00:01:57 +0200

    created custom exception

---
+++ src/main/java/com/enviro/assessment/grad001/esethuzikhali/Enviro365/errorHandling/ResourceNotFoundException.java
@@ -0,0 +1,15 @@
+package com.enviro.assessment.grad001.esethuzikhali.Enviro365.errorHandling;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResourceNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
